076e3af02313fd36a42b33c6528455af4f6c8f85
Madison Evans <m.corbinevans@gmail.com>
2023-08-13 12:36:55 -0700

    added some test logic to App.js to test custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,53 @@
+import AppContext from "./context/AppContext";
+import useDatabaseService from "./customHooks/useDatabaseService";
+
 function App() {
-	return <div>WhereTo</div>;
+	// custom hook
+	const { fetchUserGroups, fetchGroupLocations, fetchUserLocationsInGroup } =
+		useDatabaseService();
+
+	const userId = "0001";
+	const groupId = "001";
+
+	// return values from mock async requests
+	const userGroups = fetchUserGroups(userId);
+	const locations = fetchGroupLocations(groupId);
+	const userLocations = fetchUserLocationsInGroup(groupId, userId);
+
+	return (
+		<>
+			<AppContext.Provider value={userGroups}>
+				<div className="flex flex-col items-center justify-center border border-black h-screen">
+					<h1 className="text-3xl">
+						User <strong>{userId}</strong> is a member of these
+						groups:
+					</h1>
+					<ul className="my-4 text-blue-800">
+						{userGroups.map((group, i) => (
+							<li key={i}>{group.sk}</li>
+						))}
+					</ul>
+					<h1 className="text-3xl">
+						Group <strong>{groupId}</strong> has these locations:
+					</h1>
+					<ul className="my-4 text-blue-800">
+						{locations.map((location, i) => (
+							<li key={i}>{location.locationName}</li>
+						))}
+					</ul>
+					<h1 className="text-3xl">
+						User <strong>{userId}</strong> has been to the following
+						locations within Group <strong>{groupId}</strong>
+					</h1>
+					<ul className="my-4 text-blue-800">
+						{userLocations.map((userLocation, i) => (
+							<li key={i}>{userLocation.locationName}</li>
+						))}
+					</ul>
+				</div>
+			</AppContext.Provider>
+		</>
+	);
 }
 
 export default App;
