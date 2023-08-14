8ce61f3189b25eec549ecf879f98e2f143fd2430
Madison Evans <m.corbinevans@gmail.com>
2023-08-14 10:27:51 -0700

    initializing routes set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import AppContext from "./context/AppContext";
 import useDatabaseService from "./customHooks/useDatabaseService";
 import useAuthService from "./customHooks/useAuthService";
-
+import { BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./routes/Dashboard";
 function App() {
 	// custom hook for CRUD operations with database
 	const { fetchUserGroups, fetchGroupLocations, fetchUserLocationsInGroup } =
@@ -18,34 +20,11 @@ function App() {
 	return (
 		<>
 			<AppContext.Provider value={userGroups}>
-				<div className="flex flex-col items-center justify-center border border-black h-screen">
-					<h1 className="text-3xl">
-						User <strong>{userId}</strong> is a member of these
-						groups:
-					</h1>
-					<ul className="my-4 text-blue-800">
-						{userGroups.map((group, i) => (
-							<li key={i}>{group.sk}</li>
-						))}
-					</ul>
-					<h1 className="text-3xl">
-						Group <strong>{groupId}</strong> has these locations:
-					</h1>
-					<ul className="my-4 text-blue-800">
-						{locations.map((location, i) => (
-							<li key={i}>{location.locationName}</li>
-						))}
-					</ul>
-					<h1 className="text-3xl">
-						User <strong>{userId}</strong> has been to the following
-						locations within Group <strong>{groupId}</strong>
-					</h1>
-					<ul className="my-4 text-blue-800">
-						{userLocations.map((userLocation, i) => (
-							<li key={i}>{userLocation.locationName}</li>
-						))}
-					</ul>
-				</div>
+				<Router>
+					<Routes>
+						<Route path="/dashboard" element={<Dashboard />} />
+					</Routes>
+				</Router>
 			</AppContext.Provider>
 		</>
 	);
