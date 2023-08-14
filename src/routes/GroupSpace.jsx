aa0d8440bae6b24a2b8ce326186158aad65e1c52
Madison Evans <m.corbinevans@gmail.com>
2023-08-14 15:13:09 -0700

    refactoring code in GroupSpace component

--- src/routes/GroupSpace.jsx
+++ src/routes/GroupSpace.jsx
@@ -11,21 +11,25 @@ const GroupSpace = () => {
 		<div className="border h-screen grid grid-cols-[1fr,5fr]">
 			<ul className="overflow-y-auto">
 				{locations.map((location, i) => {
-					return (
-						<li
-							className="text-center border-b py-4 cursor-pointer hover:bg-gray-200"
-							key={i}
-						>
-							{location.locationName}
-						</li>
-					);
+					return <LocationCard location={location} key={i} />;
 				})}
 			</ul>
 			<div className="bg-gray-300 flex flex-col items-center justify-center">
-				<div>map goes here</div>
+				<Map />
 			</div>
 		</div>
 	);
 };
-
 export default GroupSpace;
+
+function Map() {
+	return <div>map goes here</div>;
+}
+
+function LocationCard({ location }) {
+	return (
+		<li className="text-center border-b py-4 cursor-pointer hover:bg-gray-200">
+			{location.locationName}
+		</li>
+	);
+}
