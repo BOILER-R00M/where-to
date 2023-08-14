fb34eff446dca8864bbd3a5046d8ae672eb5628d
Madison Evans <m.corbinevans@gmail.com>
2023-08-14 10:30:56 -0700

    setting up dashboard route

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 
+// Use DashboardLayout as a layout component that is responsible for structuring the children components only
 const DashboardLayout = ({ children }) => {
 	return (
 		<>
+			Dashboard
 			<div>{children}</div>
 		</>
 	);
 };
 
 const Dashboard = () => {
-	return <DashboardLayout></DashboardLayout>;
+	return (
+		<DashboardLayout>
+			{/* sub componenets of the dashboard route go here */}
+		</DashboardLayout>
+	);
 };
 
 export default Dashboard;
