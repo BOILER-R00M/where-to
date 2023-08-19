ba1ec1ec266f7c74acccf8553be4272f75e29f1b
gehrig_barnes <gehrigbarnes@gmail.com>
2023-08-19 09:46:44 -0500

    updated slide up svg. And implemeneted slide down svg. Made it so that when slideUp is true, the svg changes to the double down svg. Same thing for the p tag. When it's true, changed it to direct the user to slide down to login.

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -1,41 +1,49 @@
 import { useState } from "react";
 import LoginForm from "./LoginForm";
 import doubleUpArrow from "../../../assets/doubleUp.svg";
+import doubleDownArrow from "../../../assets/DoubleDown.svg"
 
 const Login = () => {
-    const [slideUp, setSlideUp] = useState(false);
-    const handleSlideUp = () => {
-      setSlideUp((prev)=>!prev);
-    }
-  
-    return (
-        <div className={`flex flex-col h-screen overflow-hidden ${slideUp ? 'bg-lightCoral' : 'bg-white'}`}>
-          <div className={`flex flex-col ${slideUp ? 'h-0 opacity-0' : 'flex-grow'} transition-all duration-500 overflow-hidden`}>
-            <div className="flex justify-center mx-auto overflow-auto w-81 pt-22 pb-22">
-              <h1 className="text-[7vh] font-semibold font-quicksand w-68 text-lightCoral">
-                Where To
-              </h1>
-            </div>
-            <LoginForm />
-          </div>
-          
-          <div
-            name="slideUpContainer"
-            className="flex flex-col items-center justify-center transition-all duration-500 bg-lightCoral"
-            onClick={handleSlideUp}
-          >
-            <img
-              src={doubleUpArrow}
-              alt="Double Up Arrow"
-              className="w-10 mb-4 h-9"
-            />
-            <p className="text-englishViolet font-quicksand">
-              Don't have an account? Slide up to sign up
-            </p>
-          </div>
-        </div>
-      );
-      
+  const [slideUp, setSlideUp] = useState(false);
+  const handleSlideUp = () => {
+    setSlideUp((prev) => !prev);
   };
 
-export default Login
+  return (
+    <div
+      className={`flex flex-col h-screen overflow-hidden ${
+        slideUp ? "bg-lightCoral" : "bg-white"
+      }`}
+    >
+      <div
+        className={`flex flex-col ${
+          slideUp ? "h-0 opacity-0" : "flex-grow"
+        } transition-all duration-750 overflow-hidden`}
+      >
+        <div className="flex justify-center mx-auto overflow-auto w-81 pt-22 pb-22">
+          <h1 className="text-[7vh] font-semibold font-quicksand w-68 text-lightCoral">
+            Where To
+          </h1>
+        </div>
+        <LoginForm />
+      </div>
+
+      <div
+        name="slideUpContainer"
+        className="flex flex-col items-center justify-center transition-all duration-500 bg-lightCoral"
+        onClick={handleSlideUp}
+      >
+        <img
+          src={slideUp? doubleDownArrow: doubleUpArrow}
+          alt={slideUp? "DoubleDown" : "Double Up Arrow"}
+          className="w-10 mb-4 h-9"
+        />
+        <p className="text-englishViolet font-quicksand">
+          {slideUp? "Already have an account? Slide down":"Don't have an account? Slide up to sign up"}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
