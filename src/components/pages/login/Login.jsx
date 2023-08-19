eb5c89eb4cc3d4ba721c23047d4cfb3a99674991
gehrig_barnes <gehrigbarnes@gmail.com>
2023-08-19 09:33:34 -0500

    slide up is getting better. Background is light coral. Some clipping happening when it's sliding up, also, the input fields to hide right away. I may have to adjust the timing of my animations

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -9,32 +9,33 @@ const Login = () => {
     }
   
     return (
-      <div className="flex flex-col h-screen overflow-hidden">
-        <div className={`flex flex-col ${slideUp ? 'h-0' : 'flex-grow'} transition-all duration-500 overflow-hidden`}>
-          <div className="flex justify-center mx-auto overflow-auto w-81 pt-22 pb-22">
-            <h1 className="text-[7vh] font-semibold font-quicksand w-68 text-lightCoral">
-              Where To
-            </h1>
+        <div className={`flex flex-col h-screen overflow-hidden ${slideUp ? 'bg-lightCoral' : 'bg-white'}`}>
+          <div className={`flex flex-col ${slideUp ? 'h-0 opacity-0' : 'flex-grow'} transition-all duration-500 overflow-hidden`}>
+            <div className="flex justify-center mx-auto overflow-auto w-81 pt-22 pb-22">
+              <h1 className="text-[7vh] font-semibold font-quicksand w-68 text-lightCoral">
+                Where To
+              </h1>
+            </div>
+            <LoginForm />
           </div>
-          <LoginForm />
+          
+          <div
+            name="slideUpContainer"
+            className="flex flex-col items-center justify-center transition-all duration-500 bg-lightCoral"
+            onClick={handleSlideUp}
+          >
+            <img
+              src={doubleUpArrow}
+              alt="Double Up Arrow"
+              className="w-10 mb-4 h-9"
+            />
+            <p className="text-englishViolet font-quicksand">
+              Don't have an account? Slide up to sign up
+            </p>
+          </div>
         </div>
-  
-        <div
-          name="slideUpContainer"
-          className="flex flex-col items-center justify-center transition-all duration-500 bg-lightCoral"
-          onClick={handleSlideUp}
-        >
-          <img
-            src={doubleUpArrow}
-            alt="Double Up Arrow"
-            className="w-10 mb-4 h-9"
-          />
-          <p className="text-englishViolet font-quicksand">
-            Don't have an account? Slide up to sign up
-          </p>
-        </div>
-      </div>
-    );
+      );
+      
   };
 
 export default Login
