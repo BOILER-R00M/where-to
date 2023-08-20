2986b2d183c9c0b68015a484931b9169ec120075
Madison Evans <m.corbinevans@gmail.com>
2023-08-20 12:15:27 -0700

    refactored button into a utiltiy component

--- src/components/utility/Button.jsx
+++ src/components/utility/Button.jsx
@@ -1,18 +1,29 @@
 import React from "react";
+// NOTE: While this approach may seem more verbose initially, it can lead to less code in the long run as the project grows and the components are reused.
 
 const Button = ({
 	children,
-	className,
+	variant = "primary", // Default to primary variant
 	type = "submit",
 	onClick,
 	disabled,
+	className,
 }) => {
+	// Define classes for primary and secondary variants as specified in design doc
+	const variantClasses = {
+		primary: "bg-tertiary text-primary border-tertiary",
+		secondary: "bg-primary text-tertiary border-tertiary",
+	};
+
+	// Get the classes for the specified variant (primary by default)
+	const variantClass = variantClasses[variant] || variantClasses.primary;
+
 	return (
 		<button
 			type={type}
 			onClick={onClick}
 			disabled={disabled}
-			className={`w-full px-2 py-2 text-lg text-primary cursor-pointer rounded-md font-main bg-tertiary ${className}`}
+			className={`w-full px-2 py-2 text-lg cursor-pointer rounded-md font-main ${variantClass} ${className}`}
 		>
 			{children}
 		</button>
