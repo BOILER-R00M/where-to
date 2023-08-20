cc2b9d60b4f2c298c9adac00d520c97ab5f78536
Madison Evans <m.corbinevans@gmail.com>
2023-08-20 13:09:49 -0700

    style updates

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -23,7 +23,7 @@ const GroupList = ({ groups }) => {
 	return (
 		<>
 			<Header className="pt-6 pb-3 text-tertiary">Your Groups</Header>
-			<div className="bg-none p-3 flex flex-col">
+			<div className="border border-tertiary rounded-lg bg-none p-3 flex flex-col">
 				<ul className="grid grid-col gap-3 rounded-lg">
 					{groups.map((group) => {
 						return <GroupListItem key={group.sk} group={group} />;
