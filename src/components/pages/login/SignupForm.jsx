139a20ad7839361503743b8eda5b441b898d51e6
gehrig_barnes <gehrigbarnes@gmail.com>
2023-08-21 09:42:23 -0500

    added animation to sign up form.

--- src/components/pages/login/SignupForm.jsx
+++ src/components/pages/login/SignupForm.jsx
@@ -1,15 +1,28 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import Button from "../../utility/Button";
+import { motion } from "framer-motion";
 
 // TODO:
 // [ ] place a dummy callback function for now for the login button. Will later stick the Cognito fetch function there
 
+
+
 const SignUpForm = () => {
+
+  const signUpVariant = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: { duration: 1 },
+    },
+  };
   return (
-    <div
+    <motion.div
       name="signup_form_container"
       className="max-w-[600px] mx-auto w-81 pb-[5vh] sm:pb-10 md:pb-64"
+      initial="hidden"
+      animate="visible"
+      variants={signUpVariant}
     >
       <form className="mx-auto">
         <h2 className="text-[4vh] py-3 font-semibold text-center text-secondary font-main">
@@ -72,7 +85,7 @@ const SignUpForm = () => {
       >
         Already have an account? Login
       </Link>
-    </div>
+    </motion.div>
   );
 };
 
