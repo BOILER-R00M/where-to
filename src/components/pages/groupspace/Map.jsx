192c6d1f5ddb537221ba5bfa4a1f971e8c447239
Madison Evans <m.corbinevans@gmail.com>
2023-08-22 08:53:26 -0700

    map shows score values on hover

--- src/components/pages/groupspace/Map.jsx
+++ src/components/pages/groupspace/Map.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker, Circle } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Tooltip } from "react-leaflet";
 
 const Map = ({ locations }) => {
 	// center of US as default starting point
 	const zoom = 5;
 	const position = [39.8283, -98.5795];
-
-	const handleMarkerClick = (location) => {
-		console.log(location);
-	};
 
 	return (
 		<MapContainer
@@ -21,28 +17,36 @@ const Map = ({ locations }) => {
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 			/>
 			{locations &&
-				locations.map((location) => {
-					return (
-						<div key={location.sk}>
-							<Circle
-								center={[
-									location.coordinates.lat,
-									location.coordinates.lng,
-								]}
-								radius={20}
-							/>
-							<Marker
-								position={[
-									location.coordinates.lat,
-									location.coordinates.lng,
-								]}
-								eventHandlers={{
-									click: () => handleMarkerClick(location),
+				locations.map((location) => (
+					<Marker
+						key={location.sk}
+						position={[
+							location.coordinates.lat,
+							location.coordinates.lng,
+						]}
+					>
+						<Tooltip permanent={false}>
+							<div
+								style={{
+									background: "white",
+									border: "1px solid black",
+									padding: "5px",
 								}}
-							/>
-						</div>
-					);
-				})}
+								className="border border-gray-300 bg-white"
+							>
+								<ul>
+									{Object.entries(location.averageScore).map(
+										([key, value]) => (
+											<li key={key}>
+												{key}: {value}
+											</li>
+										)
+									)}
+								</ul>
+							</div>
+						</Tooltip>
+					</Marker>
+				))}
 		</MapContainer>
 	);
 };
