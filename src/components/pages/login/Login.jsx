603e0f3a7e33ac14369c76b065d4b2c3324b6e18
gehrig_barnes <gehrigbarnes@gmail.com>
2023-08-24 09:46:24 -0500

    divs are side by side now

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -4,80 +4,82 @@ import LoginForm from "./LoginForm";
 import SignupForm from "./SignupForm";
 import doubleUpArrow from "../../../assets/doubleUp.svg";
 import doubleDownArrow from "../../../assets/DoubleDown.svg";
-// part of auth
 
 const Login = () => {
-	const [slideUp, setSlideUp] = useState(false);
-	const handleSlideUp = () => {
-		setSlideUp((prev) => !prev);
-	};
+  const [slideUp, setSlideUp] = useState(false);
+  const handleSlideUp = () => {
+    setSlideUp((prev) => !prev);
+  };
 
-	const contentVariants = {
-		hidden: {
-			height: "0%",
-			opacity: 0,
-		},
-		visible: {
-			height: "auto",
-			opacity: 1,
+  const contentVariants = {
+    hidden: {
+      height: "0%",
+      opacity: 0,
+    },
+    visible: {
+      height: "auto",
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
 
-			transition: {
-				duration: 0.5,
-			},
-		},
-	};
+  return (
+    <motion.div
+      className={`flex flex-col h-screen overflow-hidden ${
+        slideUp ? "" : "bg-primary"
+      } lg:flex-row`}
+    >
+      <motion.div
+        name="sibling"
+        variants={contentVariants}
+        initial={slideUp ? "hidden" : "visible"}
+        animate={slideUp ? "hidden" : "visible"}
+        className="flex flex-col w-1/2 overflow-hidden lg:order-1"
+      >
+        <motion.div
+          initial={{ opacity: 0, y: 0, scale: 0.8 }}
+          animate={{ opacity: 1, y: 0, scale: 1 }}
+          transition={{
+            opacity: { duration: 1, delay: 0.2, ease: "easeInOut" },
+            scale: { duration: 1, delay: 0.2, ease: "easeInOut" },
+          }}
+          exit={{ opacity: 0, y: "100vh", delay: 0 }}
+        >
+          <div className="flex justify-center mx-auto overflow-auto w-81 pt-22 pb-22 lg:w-1/2 lg:hidden">
+            <h1 className="text-[7vh] font-semibold font-main w-68 text-secondary overflow-auto scrollbar-hidden ">
+              Where To
+            </h1>
+          </div>
+        </motion.div>
+        <div className="lg:hidden">
+          <LoginForm />
+        </div>
+      </motion.div>
 
-	return (
-		<motion.div
-			className={`flex flex-col h-screen overflow-hidden ${
-				slideUp ? "" : "bg-primary"
-			}`}
-		>
-			<motion.div
-				variants={contentVariants}
-				initial={slideUp ? "hidden" : "visible"}
-				animate={slideUp ? "hidden" : "visible"}
-				className="flex flex-col overflow-hidden"
-			>
-				<motion.div
-					initial={{ opacity: 0, y: 0, scale: 0.8 }}
-					animate={{ opacity: 1, y: 0, scale: 1 }}
-					transition={{
-						opacity: { duration: 1, delay: 0.2, ease: "easeInOut" },
-						scale: { duration: 1, delay: 0.2, ease: "easeInOut" },
-					}}
-					exit={{ opacity: 0, y: "100vh", delay: 0 }}
-				>
-					<div className="flex justify-center mx-auto overflow-auto w-81 pt-22 pb-22">
-						<h1 className="text-[7vh] font-semibold font-main w-68 text-secondary overflow-auto scrollbar-hidden">
-							Where To
-						</h1>
-					</div>
-				</motion.div>
-				<LoginForm />
-			</motion.div>
-
-			<div
-				name="slideUpContainer"
-				className={`flex flex-col items-center flex-grow bg-secondary ${
-					slideUp ? "justify-start" : "justify-center"
-				}`}
-			>
-				<img
-					src={slideUp ? doubleDownArrow : doubleUpArrow}
-					alt={slideUp ? "DoubleDown" : "Double Up Arrow"}
-					className="w-10 mb-4 cursor-pointer h-9"
-					onClick={handleSlideUp}
-				/>
-				<p className="text-tertiary font-main">
-					{slideUp
-						? "Already have an account? Slide down"
-						: "Don't have an account? Slide up to sign up"}
-				</p>
-				{slideUp ? <SignupForm /> : null}
-			</div>
-		</motion.div>
-	);
+      <div
+        name="slideUpContainer"
+        className={`flex flex-col items-center flex-grow bg-secondary ${
+          slideUp ? "justify-start" : "justify-center"
+        } lg:order-2`}
+      >
+        <img
+          src={slideUp ? doubleDownArrow : doubleUpArrow}
+          alt={slideUp ? "DoubleDown" : "Double Up Arrow"}
+          className="w-10 mb-4 cursor-pointer h-9 lg:hidden"
+          onClick={handleSlideUp}
+        />
+        <p className="text-tertiary font-main lg:hidden">
+          {slideUp
+            ? "Already have an account? Slide down"
+            : "Don't have an account? Slide up to sign up"}
+        </p>
+        {slideUp ? <SignupForm /> : null}
+      </div>
+    </motion.div>
+  );
 };
 
 export default Login;
+
