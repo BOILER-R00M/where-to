c66501eb0c283baf2e625180b0aecfe85f84994a
Madison Evans <m.corbinevans@gmail.com>
2023-08-24 20:05:45 -0700

    style updates

--- src/routes/GroupSpace.jsx
+++ src/routes/GroupSpace.jsx
@@ -6,15 +6,16 @@ import Map from "../components/pages/groupspace/Map";
 import LocationCard from "../components/pages/groupspace/LocationCard";
 import upArrow from "../assets/doubleUp.svg";
 import downArrow from "../assets/doubleDown.svg";
-import { motion } from "framer-motion";
+import searchIcon from "../assets/searchIcon.svg";
 
 // TODO:
 // [x] change how the page layout looks on mobile
 // [x] Add state that allows map component to hold locations via lat/lng metadata
 // [x] render the locations on the map with a  pin or dot or some other kind of indicator
 // [x] refactor sub components into their own files
+// [x] add search bar to location list
+// [ ] add filtering functionality to search bar
 // [ ] when user clicks on location from the side bar, it should move to that location
-// [ ] add search bar to location list
 
 const GroupSpace = () => {
 	const { fetchGroupLocations } = useDatabaseService();
@@ -46,21 +47,21 @@ const LocationList = ({ locations }) => {
 	};
 	return (
 		<div
-			className={`transition-all duration-300 ease-in-out border-tertiary bg-primary z-50 absolute border border-t overflow-y-auto ${
+			className={`transition-all duration-300 ease-in-out border-tertiary bg-primary z-50 absolute border overflow-y-auto ${
 				isListHidden ? "-bottom-[336px]" : "-bottom-0"
 			} w-full bg-white lg:border-t-0 lg:overflow-y-auto lg:static h-[400px] lg:h-auto`}
 		>
 			<div className="border-b h-16 flex lg:hidden">
 				<img
 					onClick={handleShowList}
-					className="m-auto w-8 cursor-pointer"
+					className="m-auto w-8 cursor-pointer text-secondary"
 					src={isListHidden ? upArrow : downArrow}
 					alt="arrow"
 				/>
 			</div>
-			<div className="w-full flex flex-row">
-				<input type="text" className="w-full" />
-				<div>asdf</div>
+			<div className="w-full py-4 flex flex-row border-b">
+				<input type="text" className="w-full ml-4" />
+				<img src={searchIcon} alt="search" className="w-8 mx-4" />
 			</div>
 			<ul>
 				{locations
