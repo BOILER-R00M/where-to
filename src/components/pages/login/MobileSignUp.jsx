8a5f428c8a697d53b305dcacd17701929bc54d67
gehrig_barnes <gehrigbarnes@gmail.com>
2023-08-25 12:16:09 -0500

    again framer motion was giving me so much problems. I was trying to code around all the framer motion stuff I did but soon realized that I needed to start fresh with it. I finally ahve it in a place that I like

--- src/components/pages/login/MobileSignUp.jsx
+++ src/components/pages/login/MobileSignUp.jsx
@@ -1,27 +1,34 @@
+import { motion } from 'framer-motion';
 import Arrows from "../../utility/Arrows";
 import LoginForm from "./LoginForm";
 import SignupForm from "../signup/SignupForm.jsx";
 
 const MobileSignUp = ({ slideUp, handleSlideUp }) => {
-  return (
-    
-      <div
-        name="slideUpContainer"
-        className={`relative flex flex-col items-center flex-grow bg-secondary ${
-          slideUp ? "justify-start" : "justify-center"
-        } lg:hidden`}
-      >
-        
 
-        <Arrows slideUp={slideUp} handleSlideUp={handleSlideUp} />
-        <p className="text-tertiary font-main lg:hidden">
-          {slideUp
-            ? "Already have an account? Slide down"
-            : "Don't have an account? Slide up to sign up"}
-        </p>
-        {slideUp ? <SignupForm /> : null}
-      </div>
-   
+  const slideVariants = {
+    up: { height: '100%' },   // full height
+    down: { height: '20%' }   // reduced height (or whatever initial height you want)
+  };
+
+  return (
+    <motion.div
+      name="slideUpContainer"
+      className={`relative flex flex-col items-center flex-grow bg-secondary ${
+        slideUp ? "justify-start" : "justify-center"
+      } lg:hidden`}
+      variants={slideVariants}
+      initial="down"
+      animate={slideUp ? "up" : "down"}
+      transition={{ type: "spring", stiffness: 75, damping: 20 }}
+    >
+      <Arrows slideUp={slideUp} handleSlideUp={handleSlideUp} />
+      <p className="text-tertiary font-main lg:hidden">
+        {slideUp
+          ? "Already have an account? Slide down"
+          : "Don't have an account? Slide up to sign up"}
+      </p>
+      {slideUp ? <SignupForm /> : null}
+    </motion.div>
   );
 };
 
