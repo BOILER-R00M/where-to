782957c60d2ebbde9bc5bdab4833275dde73e650
gehrig_barnes <gehrigbarnes@gmail.com>
2023-09-01 08:55:02 -0500

    adding sections to the mobile view. The secon section will talk a bit more about the app

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,15 +1,51 @@
 import { useContext } from "react";
 import NavBar from "../components/ui/NavBar";
-import bus from "../assets/bus.svg"
+import bus from "../assets/bus.svg";
+
 
 export default function Home() {
   return (
     <>
       <NavBar />
-      <div >
-        <h1>Track Your Travels. Share Your Tales</h1>
-        <p>Write a little intro about the application. What it does here. something fun and cool bah blah balh</p>
-      </div>
+      <section className="h-screen bg-tertiary">
+        <div>
+          <h1 className="px-6 py-4 mx-auto text-4xl font-main text-primary ">
+            Track Your <span className="text-secondary">Travels</span>. Share Your{" "}
+            <span className="text-secondary">Tales</span>
+          </h1>
+        </div>
+
+        <div>
+          <p className="px-6 py-4 font-main text-primary">
+            Explore the world together, one pin at a time! WhereTo is a unique
+            platform that lets you create and join groups with your friends to
+            share and discover exciting travel experiences.
+          </p>
+        </div>
+        <div>
+          <img src={bus} alt="bus" />
+        </div>
+      </section>
+      <section className="h-screen bg-tertiary">
+        <div>
+          <h1 className="px-6 py-4 mx-auto text-4xl font-main text-primary ">
+            Track Your <span className="text-secondary">Travels</span>. Share Your{" "}
+            <span className="text-secondary">Tales</span>
+          </h1>
+        </div>
+
+        <div>
+          <p className="px-6 py-4 font-main text-primary">
+            Explore the world together, one pin at a time! WhereTo is a unique
+            platform that lets you create and join groups with your friends to
+            share and discover exciting travel experiences.
+          </p>
+        </div>
+        <div>
+          <img src={bus} alt="bus" />
+        </div>
+      </section>
     </>
   );
 }
+
