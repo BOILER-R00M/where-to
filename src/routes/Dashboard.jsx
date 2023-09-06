a3020366b092a92a3728a73228fc7ac37a62fc57
gehrig_barnes <gehrigbarnes@gmail.com>
2023-09-06 18:24:59 -0500

    implemented madison's login code

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+
 import { useParams } from "react-router-dom";
 import useDatabaseService from "../customHooks/useDatabaseService";
 import Header from "../components/utility/Header";
 import Display from "../components/utility/Display";
 import GroupListItem from "../components/pages/dashboard/GroupListItem";
-import Button from "../components/utility/Button";
+
 import { useContext } from "react";
 import AppContext from "../context/AppContext";
 import NavBar from "../components/ui/NavBar";
+import useAuthorization from "../customHooks/useAuthService";
 
 // TODO:
 // [x] Add username to welcome Display message
@@ -17,15 +18,16 @@ const Dashboard = () => {
   const { fetchUserGroups } = useDatabaseService();
   const groups = fetchUserGroups(userId);
   const { user } = useContext(AppContext);
+  const { userData } = useAuthorization();
+ 
 
   return (
     <>
-      <NavBar userName={user?.userName}/>
+      <NavBar userName={user?.userName} />
       <DashboardLayout
         groupList={<GroupList groups={groups} />}
-        userName={user.userName}
+        userName={userData?.userName}
       />
-      
     </>
   );
 };
@@ -34,7 +36,6 @@ const Dashboard = () => {
 const DashboardLayout = ({ userName, groupList }) => {
   return (
     <div className="relative h-screen px-20 bg-primary md:px-6 lg:px-12">
-      
       {/* <Display className="py-4 text-secondary">Welcome Back,</Display>
       <Display className="py-4 text-secondary">{userName}</Display> */}
       {groupList}
@@ -52,11 +53,10 @@ const GroupList = ({ groups }) => {
       <Header className="pt-6 pb-3 text-tertiary">My Groups: </Header>
       <div className="flex flex-col p-3 border rounded-lg border-tertiary bg-tertiary">
         <ul className="grid grid-cols-1 gap-3 rounded-lg lg:grid-cols-2">
-          {groups.map((group) => {
+          {groups?.map((group) => {
             return <GroupListItem key={group.sk} group={group} />;
           })}
         </ul>
- 
       </div>
     </div>
   );
