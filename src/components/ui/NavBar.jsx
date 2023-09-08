b408274bf7bea2800e8c713ca0613c93156eb3b3
Madison Evans <m.corbinevans@gmail.com>
2023-09-07 20:00:55 -0700

    navbar refactoring

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -1,35 +1,49 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import dropDown from "../../assets/dropDown.svg";
-import DropDown from "./DropDown";
+import useAuthorization from "../../customHooks/useAuthService";
+import NavLink from "../utility/NavLink";
+const navLinks = [
+	{ label: "Contact", action: "/contact" },
+	{ label: "Settings", action: "/settings" },
+	{ label: "Log out", action: "logout" },
+];
 
 const NavBar = ({ userName }) => {
 	const [isDropdownOpen, setDropdownOpen] = useState(false);
-
-	const toggleDropdown = () => {
-		setDropdownOpen((prevState) => !prevState);
-	};
+	const { userData } = useAuthorization();
 
 	return (
-		<nav className="box-border flex items-center justify-between w-full p-5 text-white bg-gray-800 bg-secondary">
-			<div>
-				<Link
-					to="/"
-					className="text-2xl font-bold transition-colors text-primary hover:text-gray-300 font-main"
-				>
-					WhereTo
-				</Link>
+		<nav className="bg-secondary box-border flex items-center justify-between w-full p-5 text-white bg-gray-800">
+			<Link
+				to="/"
+				className="text-2xl font-bold transition-colors text-primary hover:text-gray-300"
+			>
+				WhereTo
+			</Link>
+
+			<div className="hidden lg:flex flex-row">
+				<NavLink label={userData?.userName} action="/profile" />
+				{navLinks.map((link, index) => (
+					<NavLink key={index} {...link} />
+				))}
 			</div>
-			<ul className="relative flex gap-4">
-				{" "}
-				{/* relative for positioning dropdown */}
-				<li onClick={toggleDropdown}>
+
+			<ul className="relative flex gap-4 lg:hidden">
+				<li onClick={() => setDropdownOpen(!isDropdownOpen)}>
 					<img
 						src={dropDown}
 						alt="Dropdown Icon"
 						className="w-6 h-6 transition-opacity cursor-pointer hover:opacity-80"
 					/>
-					{isDropdownOpen && <DropDown userName={userName} />}
+					{isDropdownOpen && (
+						<div className="absolute right-0 z-50 w-48 py-2 mt-2 shadow-xl text-primary bg-tertiary top-full">
+							<NavLink label={userName} action="/profile" />
+							{navLinks.map((link, index) => (
+								<NavLink key={index} {...link} />
+							))}
+						</div>
+					)}
 				</li>
 			</ul>
 		</nav>
--- src/components/utility/NavLink.jsx
+++ src/components/utility/NavLink.jsx
@@ -1,13 +1,31 @@
 import { Link } from "react-router-dom";
+import useAuthorization from "../../customHooks/useAuthService";
 
-const NavLink = ({ tailwind = "", textContent = "", linkTo = "/" }) => {
-  return (
-    <>
-      <Link className={tailwind} to={linkTo}>
-        {textContent}
-      </Link>
-    </>
-  );
+const NavLink = ({ label, action = null, linkTo = "/" }) => {
+	const { logout } = useAuthorization();
+	const handleNavAction = (action) => {
+		if (action === "logout") {
+			logout();
+		}
+	};
+	if (action === "logout") {
+		return (
+			<button
+				onClick={() => handleNavAction(action)}
+				className="block px-4 my-auto text-gray-800 text-primary hover:bg-indigo-500 hover:text-white"
+			>
+				{label}
+			</button>
+		);
+	}
+
+	return (
+		<Link
+			to={linkTo}
+			className="block px-4 my-auto text-gray-800 text-primary hover:bg-indigo-500 hover:text-white"
+		>
+			{label}
+		</Link>
+	);
 };
-
 export default NavLink;
