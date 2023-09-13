d8248cb2c934d6633926a16739dca0645bca3910
Madison Evans <m.corbinevans@gmail.com>
2023-09-12 19:00:01 -0700

    labels added

--- src/components/pages/groupspace/AddLocationModal.jsx
+++ src/components/pages/groupspace/AddLocationModal.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import Button from "../../utility/Button";
 const AddLocationModal = ({ closeModal }) => {
+	const categories = [
+		"entertainment",
+		"culture",
+		"natural environment",
+		"cost of living",
+		"crime",
+		"pollution",
+		"food",
+		"employment opportunities",
+		"transportation",
+	];
+
 	return (
 		<div className="z-50 h-full w-full absolute">
 			<div className="border h-fit w-1/2 rounded bg-primary m-auto">
@@ -12,10 +24,22 @@ const AddLocationModal = ({ closeModal }) => {
 						action="submit"
 						className="grid grid-cols-1 gap-4 p-4"
 					>
-						<input type="text" className="border rounded p-2" />
-						<input type="text" className="border rounded p-2" />
-						<input type="text" className="border rounded p-2" />
-						<input type="text" className="border rounded p-2" />
+						{categories.map((category, index) => {
+							const id = `input-${category
+								.replace(/\s+/g, "-")
+								.toLowerCase()}`;
+							return (
+								<React.Fragment key={index}>
+									<label htmlFor={id}>{category}</label>
+									<input
+										id={id}
+										type="text"
+										className="border rounded p-2"
+										placeholder={category}
+									/>
+								</React.Fragment>
+							);
+						})}
 						<Button>Add Location</Button>
 					</form>
 				</div>
