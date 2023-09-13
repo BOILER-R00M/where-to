3ec5592926bb2247da4249d29ca5e2b36b4794f8
Madison Evans <m.corbinevans@gmail.com>
2023-09-12 17:37:25 -0700

    minor style changes

--- src/components/pages/groupspace/AddLocationModal.jsx
+++ src/components/pages/groupspace/AddLocationModal.jsx
@@ -3,7 +3,7 @@ import Button from "../../utility/Button";
 const AddLocationModal = ({ closeModal }) => {
 	return (
 		<div className="z-50 h-full w-full absolute">
-			<div className="relative border h-fit w-1/2 rounded bg-primary m-auto">
+			<div className="border h-fit w-1/2 rounded bg-primary m-auto">
 				<p className="cursor-pointer" onClick={closeModal}>
 					close
 				</p>
