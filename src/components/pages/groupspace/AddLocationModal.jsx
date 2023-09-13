7a8c6f8ec2efc7159c9893faeb7af3b3b1112110
Madison Evans <m.corbinevans@gmail.com>
2023-09-12 19:08:22 -0700

    score selections added to form

--- src/components/pages/groupspace/AddLocationModal.jsx
+++ src/components/pages/groupspace/AddLocationModal.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "../../utility/Button";
+
 const AddLocationModal = ({ closeModal }) => {
 	const categories = [
 		"entertainment",
@@ -13,36 +14,43 @@ const AddLocationModal = ({ closeModal }) => {
 		"transportation",
 	];
 
+	const scores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 	return (
 		<div className="z-50 h-full w-full absolute">
-			<div className="border h-fit w-1/2 rounded bg-primary m-auto">
+			<div className="border h-fit w-fit rounded-lg bg-primary m-auto">
 				<p className="cursor-pointer" onClick={closeModal}>
 					close
 				</p>
-				<div>
-					<form
-						action="submit"
-						className="grid grid-cols-1 gap-4 p-4"
-					>
-						{categories.map((category, index) => {
-							const id = `input-${category
-								.replace(/\s+/g, "-")
-								.toLowerCase()}`;
-							return (
-								<React.Fragment key={index}>
-									<label htmlFor={id}>{category}</label>
-									<input
-										id={id}
-										type="text"
-										className="border rounded p-2"
-										placeholder={category}
-									/>
-								</React.Fragment>
-							);
-						})}
-						<Button>Add Location</Button>
-					</form>
-				</div>
+
+				<form
+					action="submit"
+					className="grid grid-cols-1 gap-2 p-4 w-fit"
+				>
+					{categories.map((category, index) => {
+						const id = `select-${category
+							.replace(/\s+/g, "-")
+							.toLowerCase()}`;
+						return (
+							<div
+								className="flex flex-row w-full justify-between"
+								key={index}
+							>
+								<label className="pr-4" htmlFor={id}>
+									{category}
+								</label>
+								<select id={id} className="border rounded p-2">
+									{scores.map((score) => (
+										<option key={score} value={score}>
+											{score}
+										</option>
+									))}
+								</select>
+							</div>
+						);
+					})}
+					<Button>Add Location</Button>
+				</form>
 			</div>
 		</div>
 	);
